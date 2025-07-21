4b4d0e6b41afced9218e53c068af2e553e6266c8
Kunal <kunalrcciit@gmail.com>
2025-07-21 15:37:45 +0530

    get, post Postman call

--- src/app.js
+++ src/app.js
@@ -2,6 +2,14 @@ const express = require("express");
 
 const exp = express();
 
+exp.get("/user", (req, res) => {
+  res.send("This is a get call for User");
+});
+
+exp.post("/user", (req, res) => {
+  res.send("This is post call for User");
+});
+
 exp.use("/hello", (req, res) => {
   res.send("Hello, hello");
 });
